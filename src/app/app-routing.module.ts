eaa744095fd8a2a7e1cab1e00e877ceb4d746d70
Simon <simonsoft7@gmail.com>
2020-09-03 14:43:35 +0100

    Ajout du routage intermodule dans l'application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'auth', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'auth', 
+    loadChildren: './auth/auth-routing.module#AuthRoutingModule' 
+  },
+  { 
+    path: 'admin', 
+    loadChildren: './admin/admin-routing.module#AdminRoutingModule'  
+  },
+  { 
+    path: 'user', 
+    loadChildren: './user/user-routing.module#UserRoutingModule'  
+  },
+  { 
+    path: '**', 
+    redirectTo: 'auth' 
+  },
+];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
+    RouterModule,
     AuthRoutingModule
   ]
 })
